93f2586ae1cffa705f4128b8a970a3b39f2cdda9
원설아 <seolalny@gmail.com>
2023-12-04 13:21:16 +0900

    ADD :: 위험 질병

--- src/Analysis.jsx
+++ src/Analysis.jsx
@@ -10,6 +10,17 @@ function Analysis() {
     const graphImage = `images/Result/Graph/graph-${age}.png`;
     const totalGraphImage = `images/Result/Graph/graph-total.png`;
 
+    let advice = '';
+
+    if (age >= 45 && age <= 60) {
+        advice = `평균 혈색소가 13.8g/dl 이상이에요. 성인 남성은 13g/dl 미만, 성인 여성은 12g/dl미만의 혈색소를 유지해야 해요. 이를 초과하면 빈혈이 일어날 수 있으니 조심해야 해요.`;
+    } else if (age < 60) {
+        advice = "공복 혈당이 106mg/dL 이상이에요. 당뇨병을 예방하기 위해, 혈당 조절에 관심을 기울이셔야 해요.";
+    } else if (age == 80) {
+        advice = "80세는 평균 공복 혈당이 108.6mg/dL이에요. 공복 혈당이 126mg/dL 이상이 되면 당뇨병이 의심되니 조심해야 해요.";
+    }
+
+
     return (
         <ContentBox>
             <GlobalStyle />
@@ -22,7 +33,9 @@ function Analysis() {
                 <img src={totalGraphImage} alt="전체 평균 그래프" />
             </>
             <>
-                <StyledP>공복 혈당이 126mg/dL 이상이라면 <b>당뇨병</b>이 의심될 수 있어요.</StyledP>
+                <StyledP>
+                    {advice}
+                </StyledP>
             </>
         </ContentBox>
     )
